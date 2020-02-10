b2321bb0cff39a4e082e347bb642faf8988f959b
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-10 10:25:22 -0700

    Update index action to render locals for view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    connection = Faraday.new('https://www.potterapi.com/v1/') do |f|
-      f.adapter Faraday.default_adapter
-    end
-
-    response = connection.get("characters?key=#{ENV['POTTER_API_KEY']}&house=Gryffindor")
-
-    gryffindor_members = JSON.parse(response.body, symbolize_names: true)
-
-    order_members = gryffindor_members.find_all do |member_info|
-      member_info[:orderOfThePhoenix] == true
-    end
-
-    @members = order_members.map do |member_info|
-      Member.new(member_info)
-    end
+    render locals: {
+      members: SearchResultsFacade.new.members("Gryffindor")
+    }
   end
 end
