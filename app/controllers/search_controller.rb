f9f347f99309cdac8573494ef289a735b4107232
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-10 10:27:43 -0700

    Update index action to use params[:house] when searching for members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     render locals: {
-      members: SearchResultsFacade.new.members("Gryffindor")
+      members: SearchResultsFacade.new.members(params[:house])
     }
   end
 end
