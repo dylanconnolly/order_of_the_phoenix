43031485847e6f09d4f87ea2c2ea0eb5ad9ed579
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-10 09:49:33 -0700

    Create member class to create objects for each order member

---
+++ app/models/member.rb
@@ -0,0 +1,9 @@
+class Member
+
+  def initialize(member_info)
+    @name = member_info[:name]
+    @role = member_info[:role]
+    @house = member_info[:house]
+    @patronus = member_info[:patronus]
+  end
+end
