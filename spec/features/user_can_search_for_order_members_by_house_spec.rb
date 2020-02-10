ddc078b98c04c16dc9dfff80af1f9345dcbe0596
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-10 09:30:11 -0700

    Create spec for search functionality

---
+++ spec/features/user_can_search_for_order_members_by_house_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "as a user" do
+  it "when a house is selected from the dropdown, the user sees a list of members for that house" do
+
+    visit '/'
+
+    select "Gryffindor", from: :house
+
+    click_on "Search For Members"
+
+    expect(current_path).to eq(search_path)
+
+    expect(page).to have_css('.order-member', count: 21)
+
+    within(first('.order-member') do
+      expect(page).to have_css('.name')
+      expect(page).to have_css('.role')
+      expect(page).to have_css('.house')
+      expect(page).to have_css('.patronus')
+    end
+  end
+end
