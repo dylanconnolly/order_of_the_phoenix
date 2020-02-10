87b86c62000e40fe97b79e425b5242a6ba1039a9
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-10 10:26:14 -0700

    Have order_members method return member objects

--- app/poros/house_members.rb
+++ app/poros/house_members.rb
@@ -3,8 +3,12 @@ class HouseMembers
   def order_members(house)
     all_house_characters = PotterService.new.get_characters_by_house(house)
 
-    all_house_characters.find_all do |character_info|
+    members = all_house_characters.find_all do |character_info|
       character_info[:orderOfThePhoenix] == true
+    end
+
+    members.map do |member_info|
+      Member.new(member_info)
     end
   end
 end
